e319c9d1dbd98d794e584e1eb092f2ce6f279f4d
mhuntno <me@mhunt.no>
2026-02-18 15:28:09 +0100

    add generation script for dart

--- scripts/generate_dart.ps1
+++ scripts/generate_dart.ps1
@@ -1,5 +1,5 @@
 $PROTO_DIR = "./skavl-proto/proto"
-$OUT_DIR = "./lib/skavl-proto"
+$OUT_DIR = "./lib/proto"
 
 protoc `
   --proto_path=$PROTO_DIR `
--- scripts/generate_dart.sh
+++ scripts/generate_dart.sh
@@ -2,7 +2,7 @@
 set -e
 
 PROTO_DIR="./skavl-proto/proto"
-OUT_DIR="./lib/skavl-proto"
+OUT_DIR="./lib/proto"
 
 protoc \
   --proto_path="$PROTO_DIR" \
