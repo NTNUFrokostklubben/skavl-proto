d7e89206268e81c8a17b0f2bf23553b6b4af7ff5
mhuntno <me@mhunt.no>
2026-02-18 15:49:34 +0100

    update generation script to include grpc service headers

--- scripts/generate_dart.ps1
+++ scripts/generate_dart.ps1
@@ -3,5 +3,5 @@ $OUT_DIR = "./lib/proto"
 
 protoc `
   --proto_path=$PROTO_DIR `
-  --dart_out=$OUT_DIR `
+  --dart_out=grpc:$OUT_DIR `
   $PROTO_DIR/*.proto
--- scripts/generate_dart.sh
+++ scripts/generate_dart.sh
@@ -6,5 +6,5 @@ OUT_DIR="./lib/proto"
 
 protoc \
   --proto_path="$PROTO_DIR" \
-  --dart_out="$OUT_DIR" \
+  --dart_out=grpc:"$OUT_DIR" \
   "$PROTO_DIR"/*.proto
